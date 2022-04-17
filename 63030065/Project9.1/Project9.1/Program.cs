8332cc546d8f25ddefc3ded7abfe852b4e1859fc
Tobio <92078869+Pimlapus@users.noreply.github.com>
2022-04-17 17:00:36 +0700

    addFolder

---
+++ 63030065/Project9.1/Project9.1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Project9._1
+{
+    class MyClass
+    {
+        // Non numeric = 2 types
+        public bool myBool;
+        public char myChar;
+
+        // Integer = 8 types
+        public byte myByte;
+        public sbyte mySbyte;
+        public short myShort;
+        public ushort myUShort;
+        public int myInt;
+        public uint myUInt;
+        public long myLong;
+        public ulong myULong;
+
+        // Floating point = 3 types
+        public float myFloat;
+        public double myDouble;
+        public decimal myDecimal;
+
+        // Others = 3 types
+        public string myString;
+        public object myObject;
+        public dynamic myDynamic;
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass mc = new MyClass();
+            Console.WriteLine("Non numeric types default value.");
+            Console.WriteLine($"Default value of Bool   \t: {mc.myBool}");
+            Console.WriteLine($"Default value of Char   \t: {mc.myChar}");
+            Console.WriteLine($"-------------------------");
+            Console.WriteLine("Integer types default value.");
+            Console.WriteLine($"Default value of Sbyte  \t: {mc.mySbyte}");
+            Console.WriteLine($"Default value of Byte   \t: {mc.myByte}");
+            Console.WriteLine($"Default value of Short  \t: {mc.myShort}");
+            Console.WriteLine($"Default value of UShort \t: {mc.myUShort}");
+            Console.WriteLine($"Default value of Int    \t: {mc.myInt}");
+            Console.WriteLine($"Default value of UInt   \t: {mc.myUInt}");
+            Console.WriteLine($"Default value of Long   \t: {mc.myLong}");
+            Console.WriteLine($"Default value of ULong  \t: {mc.myULong}");
+            Console.WriteLine($"-------------------------");
+            Console.WriteLine("Floating point types default value.");
+            Console.WriteLine($"Default value of Float  \t: {mc.myFloat}");
+            Console.WriteLine($"Default value of Double \t: {mc.myDouble}");
+            Console.WriteLine($"Default value of Decimal\t: {mc.myDecimal}");
+            Console.WriteLine($"-------------------------");
+            Console.WriteLine("Others predefined types default value.");
+            Console.WriteLine($"Default value of String \t: {mc.myString}");
+            Console.WriteLine($"Default value of Object \t: {mc.myObject}");
+            Console.WriteLine($"Default value of Dynamic\t: {mc.myDynamic}");
+        }
+    }
+}
